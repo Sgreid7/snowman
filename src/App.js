36f829ee54900edc7d6a38f1ed9c44e434a1cfa3
Sam Greider <sam.greider7@gmail.com>
2020-03-24 16:28:57 -0400

    added hooks for app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { Component, useState, useEffect } from 'react'
+import words from './data.json'
+import alphabet from './letters.json'
 
 const App = () => {
-  return <HelloWorld />
+  // set hooks
+  const [clickedLetters, setClickedLetters] = useState([])
+  const [randomWord, setRandomWord] = useState('')
+  const [revealedLetters, setRevealedLetters] = useState([])
+
+  useEffect(() => {
+    // when component renders, set a random word
+    setRandomWord(words[Math.ceil(Math.random() * words.length)])
+  }, [])
+
+  const letters = randomWord.split('')
+
+  const onLetterClicked = (e) => {
+    // disable the button
+    e.target.disabled = true
+    const value = e.target.value
+    setClickedLetters((oldLetter) => [...oldLetter, value])
+    if (letters.includes(value.toLowerCase())) {
+      // set revealed letters
+      setRevealedLetters((oldLetter) => [...oldLetter, value])
+    }
+  }
+
+  return (
+    <>
+      <main>
+        <section className="word">
+          {letters.map((letter) => {
+            return (
+              <div className="space">
+                <div
+                  className={
+                    clickedLetters.includes(letter.toUpperCase())
+                      ? ''
+                      : 'hiddenLetter'
+                  }
+                  key={letter}
+                >
+                  {letter}
+                </div>
+              </div>
+            )
+          })}
+        </section>
+        <section className="buttons">
+          {alphabet.map((letter) => {
+            return (
+              <button
+                onClick={onLetterClicked}
+                className="button"
+                key={letter}
+                value={letter}
+              >
+                {letter}
+              </button>
+            )
+          })}
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
